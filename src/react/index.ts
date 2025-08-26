1636342b1c4bde526041c846542582560598b0fe
SothisKrieg <ftatilon@gmail.com>
2025-08-26 15:17:41 +0200

    Removed drop sub component

--- src/react/index.ts
+++ src/react/index.ts
@@ -4,6 +4,5 @@ export { ChunkifyUploaderProgressBar } from './ChunkifyUploaderProgressBar';
 export { ChunkifyUploaderFileSelect } from './ChunkifyUploaderFileSelect';
 export { ChunkifyUploaderError } from './ChunkifyUploaderError';
 export { ChunkifyUploaderSuccess } from './ChunkifyUploaderSuccess';
-export { ChunkifyUploaderDrop } from './ChunkifyUploaderDrop';
 export { ChunkifyUploaderHeading } from './ChunkifyUploaderHeading';
 export { ChunkifyUploaderRetry } from './ChunkifyUploaderRetry';
